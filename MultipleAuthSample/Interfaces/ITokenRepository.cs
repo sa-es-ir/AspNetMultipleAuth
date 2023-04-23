e742339539171d19e1c7ad14e572db0645054e66
sa-es-ir <s.esmaeelinejad@gmail.com>
2023-04-23 16:35:46 +0700

    add token repository interface

---
+++ MultipleAuthSample/Interfaces/ITokenRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace MultipleAuthSample.Interfaces;
+
+/// <summary>
+/// you need to implement your own logic
+/// </summary>
+public interface ITokenRepository
+{
+    Task<CustomToken> GetAsync(string customToken);
+}
+
+public class CustomToken
+{
+    public string UserId { get; set; } = string.Empty;
+
+    public string Token { get; set; } = string.Empty;
+}
