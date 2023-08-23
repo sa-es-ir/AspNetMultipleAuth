7652a8cc4e28277ec065243b05d9081464ebf867
sa-es-ir <s.esmaeelinejad@gmail.com>
2023-08-23 21:40:40 +0700

    clean program.cs

--- MultipleAuthSample/Program.cs
+++ MultipleAuthSample/Program.cs
@@ -1,75 +1,21 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.Net.Http.Headers;
 using Microsoft.OpenApi.Models;
-using MultipleAuthSample.AuthHandlers;
+using MultipleAuthSample;
 using MultipleAuthSample.Interfaces;
-using System.IdentityModel.Tokens.Jwt;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddScoped<ITokenRepository, TokenRepository>();
-
 var config = builder.Configuration;
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    //the scheme name is JwtBearerDefaults.AuthenticationScheme and this is the default and all requests go to this scheme
-    // and then redirect to appropriate authentication scheme
-    .AddPolicyScheme(JwtBearerDefaults.AuthenticationScheme, JwtBearerDefaults.AuthenticationScheme, options =>
-     {
-         options.ForwardDefaultSelector = context =>
-         {
-             var jwtHandler = new JwtSecurityTokenHandler();
-             var token = context.Request.Headers[HeaderNames.Authorization];
-             if (!string.IsNullOrEmpty(token) && jwtHandler.CanReadToken(token))
-             {
-                 var tokenIssuer = jwtHandler.ReadJwtToken(token).Issuer;
+builder.Services.SetupAuthenticationFirst(config);
+//builder.Services.SetupAuthenticationSecond(config);
 
-                 if (tokenIssuer == config.GetValue<string>("IdentitySeverA:Issuer"))
-                     return "Scheme_ServerA";
-                 else
-                     return "Scheme_ServerB";
-             }
-
-             return "CustomToken";
-         };
-     })
-  //IdentityServerA, the Scheme name is Scheme_ServerA
-  .AddJwtBearer("Scheme_ServerA", options =>
-  {
-      options.TokenValidationParameters = new TokenValidationParameters
-      {
-          ValidAudience = config.GetValue<string>("IdentitySeverA:Audience"),
-          ValidIssuer = config.GetValue<string>("IdentitySeverA:Issuer"),
-          IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("A-Secret-Key:)"))
-      };
-  })
-  //IdentityServerB, the Scheme name is Scheme_ServerB
-  .AddJwtBearer("Scheme_ServerB", options =>
-  {
-      options.TokenValidationParameters = new TokenValidationParameters
-      {
-          ValidAudience = config.GetValue<string>("IdentitySeverB:Audience"),
-          ValidIssuer = config.GetValue<string>("IdentitySeverB:Issuer"),
-          IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("B-Secret-Key:)"))
-      };
-  })
-  //The scheme name is CustomToken
-  .AddScheme<CustomAuthSchemeOptions, CustomAuthenticationHandler>("CustomToken", options =>
-  {
-      // no need to set any options because you will handle them in the handler implementation
-  });
-
+#region OtherSetup
 builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 
+// Add services to the container.
+builder.Services.AddScoped<ITokenRepository, TokenRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -118,3 +64,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+#endregion
--- MultipleAuthSample/WeatherForecast.cs
+++ MultipleAuthSample/WeatherForecast.cs
@@ -11,3 +11,20 @@ namespace MultipleAuthSample
         public string? Summary { get; set; }
     }
 }
+
+
+//var tokenHandler = new JwtSecurityTokenHandler();
+//var tokenDescriptor = new SecurityTokenDescriptor
+//{
+//    Subject = new ClaimsIdentity(new[] { new Claim("sub", Guid.NewGuid().ToString()) }),
+//    Claims = new Dictionary<string, object>
+//            {
+//                { "Roles", "Admin" },
+//            },
+//    Issuer = config.GetValue<string>("IdentitySeverA:Issuer"),
+//    Audience = config.GetValue<string>("IdentitySeverA:Audience"),
+//    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SomeThingThatShouldNotBeHereA"))
+//    , SecurityAlgorithms.HmacSha256Signature)
+//};
+//var token = tokenHandler.CreateToken(tokenDescriptor);
+//var tokenff = tokenHandler.WriteToken(token);
